699e025db4644f0aa29ded3fc8577bc0e0a3013e
sanghun lee <57477415+sanghunlee-711@users.noreply.github.com>
2020-06-25 00:32:18 +0900

    BrushsizeAdjustment
    Done

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
 //canvas는 내부 pixel을 다룰수 있는 자주 사용되는 요소임
 
 canvas.width = 700;
@@ -9,6 +12,7 @@ ctx.strokeStyle = "black";
 ctx.lineWidth = 2.5;
 
 let painting =false;
+let filling = false;
 
 function stopPainting(){
     painting = false;
@@ -37,7 +41,26 @@ function onmouseDown(event){
     stopPainting();
 }
 
+function handleColorClick(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color; //strokestyle의 default설정값을 클릭시 생성되는 event내의 background컬러를 바꿔줌으로서 색칠할 색이 바뀌게됨.
+}
 
+function handleRangechange(event){
+    const size = event.target.value;
+    ctx.lineWidth = size;
+}
+
+function handleModeClick(){
+    if(filling === true){
+        filling = false;
+        mode.innerText = "Fill";
+    }
+    else{
+        filling = true;
+        mode.innerText = "Paint";
+    }
+}
 
 //Event 내에 보면 client x,y와 offset x,y가 존재하는데 client x,y는 화면전체의 크기 offset x,y는 내가 selecting한 크기(캔버스크기)가 되므로
 //event내에서 offset x,y를 가져와야한다.
@@ -46,4 +69,17 @@ if(canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+}
+
+
+//array.from method는 object로부터 array를 만듦.
+Array.from(colors).forEach(controled_color => controled_color.addEventListener("click", handleColorClick));
+//controled_color는 array로바꾼 내부에 있는 하나하나의 아이템을 말하는 것이기에 무슨 명을 사용해도 상관없음 !
+
+if(range){
+    range.addEventListener("input", handleRangechange);
+}
+
+if(mode){
+    mode.addEventListener("click", handleModeClick);
 }
